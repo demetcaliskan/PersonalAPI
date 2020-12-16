90685f91fc4baecefbf2416f5531d7f3ab133b44
demetcaliskan <demetcaliskann@gmail.com>
2020-12-16 03:50:56 +0300

    chained route handlers using express

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
@@ -22,40 +24,38 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
-  Article.find(function(err, results) {
-    if(!err) {
-      res.send(results);
-    } else {
-      res.send(err);
-    }
+app.route("/articles")
+  .get(function(req, res) {
+    Article.find(function(err, results) {
+      if (!err) {
+        res.send(results);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully added a new article!");
+      }
+    });
+  })
+  .delete(function(req, res) {
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all the articles!");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
-
-app.post("/articles", function(req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newArticle.save(function(err) {
-    if(err) {
-      res.send(err);
-    } else {
-      res.send("Successfully added a new article!");
-    }
-  });
-});
-
-app.delete("/articles", function(req, res) {
-  Article.deleteMany(function(err) {
-    if(!err) {
-      res.send("Successfully deleted all the articles!");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
